9ebc1b5169d96e20797762ea1eb7403a9550a4d3
Erich Merrill <erich@erichmerrill.com>
2019-11-29 18:06:23 -0800

    Added explicit modelnet point cloud loader

--- kaolin/datasets/modelnet.py
+++ kaolin/datasets/modelnet.py
@@ -109,6 +109,68 @@ class ModelNet(torch.utils.data.Dataset):
             raise NotImplementedError
 
 
+class ModelNetPointCloud(torch.utils.data.Dataset):
+    def __init__(self, basedir: Union[str, Path],
+                 split: Optional[str] = 'train',
+                 categories: Optional[Iterable] = None,
+                 device: Optional[Union[torch.device, str]] = 'cpu',
+                 transform: Optional[Callable] = None,
+                 **kwargs):
+        super(ModelNet, self).__init__()
+
+        if rep not in ['mesh', 'pointcloud']:
+            raise ValueError('Argument \'rep\' must be one of \'mesh\' '
+                ' or \'pointcloud\'. Got {0} instead.'.format(rep))
+        if split not in ['train', 'test']:
+            raise ValueError('Argument \'split\' must be one of \'train\' '
+                ' or \'test\'. Got {0} instead.'.format(split))
+
+        if not isinstance(basedir, Path):
+            basedir = Path(basedir)
+
+        # Find categories provided by the dataset
+        avail_categories = [d.name for d in basedir.iterdir() if d.is_dir()]
+        self.categories = categories or avail_categories
+        self.paths = []
+        self.labels = []
+        for idx, cat in enumerate(self.categories):
+            if cat not in avail_categories:
+                raise ValueError('Invalid ModelNet class {0}. Valid classes '
+                    ' are {1}'.format(cat, avail_categories))
+            catdir = basedir / cat / split
+            for path in catdir.glob('*.off'):
+                self.paths.append(path)
+                self.labels.append(idx)
+
+        self.rep = rep
+        self.device = device
+        self.transform = transform
+        self.num_points = kwargs.get('num_points', 1024)
+        self.sample_points = kwargs.get('sample_points', self.num_points)
+        self.sample_cache = dict()
+
+    def __len__(self):
+        r"""Returns the length of the dataset. """
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        r"""Returns the item at index `idx`. """
+        if idx not in self.sample_cache:
+            mesh = kal.rep.TriangleMesh.from_off(self.paths[idx])
+            mesh.to(self.device)
+            pts, _ = mesh.sample(self.sample_points)
+            self.sample_cache[idx] = pts
+
+        full_pts = self.sample_cache[idx]
+        # Downsample points
+        idxs = np.random.choice(pts.size(0), size=self.num_points,
+                                replace=False)
+        pts = full_pts[idxs, :]
+        if self.transform is not None:
+            pts = self.transform(pts)
+        return pts, label
+
+
 class ModelNetVoxels(object):
     r""" Dataloader for downloading and reading from ModelNet.
 
